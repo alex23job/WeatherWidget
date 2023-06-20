23ce7182f79ea44ffe27efebdcc8e180388f8093
alex23job <106354657+alex23job@users.noreply.github.com>
2023-06-20 16:04:39 +0300

    End_L3

---
+++ scripts/widget.js
@@ -0,0 +1,9 @@
+import { startWidget } from "./modules/widgetService.js";
+
+const init = (app) => {
+    const widget = startWidget();
+
+    app.append(widget);
+}
+
+init(document.querySelector('#app'));
